3b385d6b980b6b553def0096f6956951a48dddb1
Rafiul789 <89851417+Rafiul789@users.noreply.github.com>
2022-04-04 13:52:40 +0600

    navbar and headeradded

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import logo from './logo.svg';
+
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './Components/Home/Home';
+import Navbar from './Components/Navbar/Navbar';
+import Reviews from './Components/Reviews/Reviews';
+
+import Blogs from './Components/Blogs/Blogs';
+import About from './Components/About/About';
+import Dashboard from './Components/Dashboard/Dashboard';
 
 function App() {
   return (
     <div >
-     <h1>Hello welcome to fruit corner</h1>
+<Navbar></Navbar>
+
+     <Routes> <Route path='/' element={<Home></Home>}  > </Route>  <Route path='/reviews' element={<Reviews></Reviews>}  > </Route>   <Route path='/dashboard' element={<Dashboard></Dashboard>}  > </Route>   <Route path='/blogs' element={<Blogs></Blogs>}  > </Route>   <Route path='/about' element={<About></About>}  > </Route>   </Routes>
     </div>
   );
 }
---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import { NavLink } from "react-router-dom";
+const Navbar = () => {
+    return (
+        <nav  className="flex  gap-2 justify-center md:justify-between  py-5 px-5">
+        <div className='text-4xl  text-blue-600'>
+          <h1>Fruit Corner</h1>
+        </div>
+        <div className='text-xl flex gap-3'>
+          <NavLink
+             style={isActive => ({
+              color: isActive ? "blue" : "green"
+            })}
+            to='/'
+          >
+            Home
+          </NavLink>
+          <NavLink
+                  style={isActive => ({
+                    color: isActive ? "blue" : "green"
+                  })}      to='/reviews'
+          >
+            Reviews
+          </NavLink>
+          <NavLink
+             style={isActive => ({
+              color: isActive ? "blue" : "green"
+            })}
+            to='/dashboard'
+          >
+            Dashboard
+          </NavLink>
+
+          <NavLink
+              style={isActive => ({
+                color: isActive ? "blue" : "green"
+              })}
+            to='/blogs'
+          >
+            Blogs
+          </NavLink>
+
+          <NavLink
+            style={isActive => ({
+              color: isActive ? "blue" : "green"
+            })}
+           
+            to='/about'
+          >
+            about
+          </NavLink>
+        </div>
+      </nav>
+    );
+};
+
+export default Navbar;
