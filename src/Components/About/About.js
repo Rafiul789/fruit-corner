d1f864bb5324e6b111878864f5c6c864472ee5ff
Rafiul789 <89851417+Rafiul789@users.noreply.github.com>
2022-04-04 20:26:27 +0600

    about section added

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -3,7 +3,24 @@ import React from 'react';
 const About = () => {
     return (
         <div>
-            <h1>About us</h1>
+               <h1  className="text-4xl text-center text-blue-500">About us</h1>  
+
+<p className="text-xl my-3" >  Welcome to Fruit Corner, your number one source for all fruits. We're dedicated to providing you the very best of fruits,we have fresh and premium products.
+
+
+Founded in 1999 by Habibur Rahman, Fruit Corner has come a long way from its beginnings in London. When Habibur Rahman first started out, his passion for  "eco-friendly cleaning products" drove them to start their own business.
+
+
+We hope you enjoy our products as much as we enjoy offering them to you. If you have any questions or comments, please don't hesitate to contact us.
+
+
+
+ </p>  
+ <h1 className="text-2xl">  Sincerely, <br></br>
+Rafiul Islam  <br></br>
+
+Founder of Fruit Corner  </h1>
+
         </div>
     );
 };
