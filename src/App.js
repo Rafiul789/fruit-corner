99a7c977ef2bdeed36a9113e319649fee27daffb
Rafiul789 <89851417+Rafiul789@users.noreply.github.com>
2022-04-04 15:15:29 +0600

    not found page added

--- src/App.js
+++ src/App.js
@@ -8,13 +8,14 @@ import Reviews from './Components/Reviews/Reviews';
 import Blogs from './Components/Blogs/Blogs';
 import About from './Components/About/About';
 import Dashboard from './Components/Dashboard/Dashboard';
+import NotFound from './Components/NotFound/NotFound';
 
 function App() {
   return (
     <div >
 <Navbar></Navbar>
 
-     <Routes> <Route path='/' element={<Home></Home>}  > </Route>  <Route path='/reviews' element={<Reviews></Reviews>}  > </Route>   <Route path='/dashboard' element={<Dashboard></Dashboard>}  > </Route>   <Route path='/blogs' element={<Blogs></Blogs>}  > </Route>   <Route path='/about' element={<About></About>}  > </Route>   </Routes>
+     <Routes> <Route path='/' element={<Home></Home>}  > </Route>  <Route path='/reviews' element={<Reviews></Reviews>}  > </Route>   <Route path='/dashboard' element={<Dashboard></Dashboard>}  > </Route>   <Route path='/blogs' element={<Blogs></Blogs>}  > </Route>   <Route path='/about' element={<About></About>}  > </Route> <Route path='*' element={<NotFound></NotFound>} ></Route> </Routes>
     </div>
   );
 }
---
+++ src/Components/NotFound/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import image from './error.jpg'
+
+const NotFound = () => {
+    return (
+        <div>
+            <img src={image} alt=""></img>
+        </div>
+    );
+};
+
+export default NotFound;
